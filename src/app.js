d0a4a2910dea3a4d0416c577d11c530e0c637b4b
Valentina Pereyra Castillo <valepereyracastillo@gmail.com>
2023-01-11 19:09:12 +0000

    Random card generetoor funcionando

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♣", "♠", "♥", "♦"];
+  let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+  let sorteoPalo = Math.floor(Math.random() * palo.length);
+  let sorteoNumber = Math.floor(Math.random() * number.length);
+
+  let contenedorNumber = number[sorteoNumber];
+  let contenedorPalo = palo[sorteoPalo];
+
+  let color = contenedorPalo == "♦" || contenedorPalo == "♥" ? "red" : "black";
+
+  document.getElementById("header").innerHTML = contenedorPalo;
+  document.getElementById("header").style.color = color;
+  document.getElementById("body").innerHTML = contenedorNumber;
+  document.getElementById("body").style.color = color;
+  document.getElementById("footer").innerHTML = contenedorPalo;
+  document.getElementById("footer").style.color = color;
 };
